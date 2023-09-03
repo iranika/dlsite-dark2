81a1dc90014be813a4cf08a3a5b3ae41882f7518
iranika <7370486+iranika@users.noreply.github.com>
2023-09-03 19:40:51 +0900

    ✨ギフトボタンの追加

--- src-bex/my-content-script.ts
+++ src-bex/my-content-script.ts
@@ -14,9 +14,12 @@ export default bexContent(( bridge ) => {
 
   const html = document.querySelector('html');
   if (html != null){
-    html.style.filter = 'brightness(0.7)';
     chrome.storage.local.get('bright', (items) => {
       // Group the values up into an array to take advantage of the bridge's chunk splitting.
+      if (!items['bright']){
+        html.style.filter = 'brightness(0.9)';
+        return;
+      }
       html.style.filter = `brightness(${items})`;
     });
     bridge.on('dd.update', ({data, respond}) => {
@@ -24,6 +27,27 @@ export default bexContent(( bridge ) => {
       html.style.filter = `brightness(${data.bright})`;
       respond();
     });
+
+    chrome.storage.local.get('gift_button', (item) => {
+      if (!item['gift_button'] || item['gift_button'] == false) return;
+
+      if (location.href.includes('/work/')){
+        const boxElm = document.querySelector('#work_buy_btn'); //ボタンの表示位置
+        const hasWork = document.querySelector('#work_buy_btn > p.work_stream > a'); //購入済み判定
+
+        if (hasWork && boxElm){
+          const workId = location.href.replace(/.*\//, '').replace(/\.html.*$/, '');
+
+          boxElm.innerHTML += `
+        <div class="work_favorite">
+          <a href="https://www.dlsite.com/home/mypage/buy/dlgift/product/=/product_id/${workId}/" class="btn_gift">
+            ギフト券を購入
+          </a>
+        </div>
+        `
+        }
+      }
+    })
   }
-  
+
 })
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    
-    <q-card style="width:800px">
+    <div style="width:800px">
+      <q-card style="width: 100%;">
       <q-card-section>
         <div>明るさ</div>
         <q-slider
@@ -22,7 +22,14 @@
         -->
       </q-card-actions>
     </q-card>
-    
+
+    <q-card style="width:100%; margin-top: 10px;">
+      <q-card-section>
+        <div>ギフトページへのアクセスボタン</div>
+        <q-checkbox @click="sendSave" v-model="gift_button" label="作品ページにボタンを表示する"></q-checkbox>
+      </q-card-section>
+    </q-card>
+    </div>
   </q-page>
 </template>
 
@@ -38,13 +45,21 @@ export default defineComponent({
     const sendSave = async()=>{
       //$q.bex.send('log', { message: 'Hello World!' });
       const bright = await $q.bex.send('storage.set', { key: 'bright', value: brightness.value });
-      console.log('bright:',bright)
+      console.log('bright:',bright);
+      const gift = await $q.bex.send('storage.set', { key: 'gift_button', value: gift_button.value });
+      console.log('gift', gift);
     }
-    let brightness = ref(0.7);
+    let brightness = ref(0.9);
+    let gift_button = ref(false);
 
     $q.bex.send('storage.get', {key: 'bright'}).then((res)=>{
-      brightness.value = res.data
+      brightness.value = (res.data == undefined) ? 0.9 : res.data;
       console.log('get bright:', res.data);
+    })
+
+    $q.bex.send('storage.get', {key: 'gift_button'}).then((res)=>{
+      gift_button.value = (res.data == undefined) ? false : res.data;
+      console.log('get gift_button:', res.data);
     })
 
     const saveDefault = async ()=>{
@@ -52,7 +67,7 @@ export default defineComponent({
       sendSave();
     }
 
-    return { sendSave, brightness, saveDefault };
+    return { sendSave, brightness, saveDefault, gift_button };
   }
 });
 </script>
